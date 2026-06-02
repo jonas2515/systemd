c11c5517274a45318fce79c8e30de9727a0ec957
Jonas Dreßler <verdre@v0yd.nl>
2026-06-02 15:58:38 +0200

    Reapply "finally"
    This reverts commit 5324dc5aff886961145e6b6c5baef6d724a0f2f2.

--- src/repart/repart.c
+++ src/repart/repart.c
@@ -1643,10 +1643,10 @@ static int context_grow_partitions_on_free_area(Context *context, FreeArea *a) {
 
                 if (last_partition) {
                         assert(last_partition->new_padding != UINT64_MAX);
-                        last_partition->new_padding += span;
+                        last_partition->new_padding += round_down_size(span, context->grain_size);
                 } else if (a->after) {
                         assert(a->after->new_padding != UINT64_MAX);
-                        a->after->new_padding += span;
+                        a->after->new_padding += round_down_size(span, context->grain_size);
                 }
         }
 
